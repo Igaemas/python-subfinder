6f27d8be8fe59d2b166267669bc8950b733c2542
igaemas <sasha.deroubaix.dev@proton.me>
2023-02-16 18:04:26 +0100

    mis au propre

--- subfinder.py
+++ subfinder.py
@@ -26,13 +26,16 @@ class colors:
     RED = '\033[91m'
     RESET = '\033[0m'
 
+wordlist = sys.argv[2]
+main_domain = sys.argv[1]
+
 def progress_bar(progress, total):
     percent = 100 * (progress / float(total))
     # caracter = ALT + 219
     bar = '█' * int(percent) + ' ' * (100 - int(percent))
     print(f"\r|{bar}| {percent:2f}%", end="\r")
 
-sub_list = open(sys.argv[2]).read()
+sub_list = open(wordlist).read()
 subdoms = sub_list.splitlines()
 progress_int = 0
 sub_number = len(subdoms) 
@@ -40,7 +43,7 @@ valid_domain_list = []
 
 for sub in subdoms:
     progress_int += 1
-    sub_domain = f"http://{sub}.{sys.argv[1]}"
+    sub_domain = f"http://{sub}.{main_domain}"
 
     if len(sys.argv) > 3:
         pass
@@ -66,4 +69,6 @@ for sub in subdoms:
 if len(valid_domain_list) == 0:
     print("no valid domain found")
 else:
-    print(valid_domain_list)
+    print(f'There are {len(valid_domain_list)} valid sub domain: ')
+    for valid_domain in valid_domain_list:
+        print('   ' + valid_domain)
